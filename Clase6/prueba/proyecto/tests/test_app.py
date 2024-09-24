41acae7542c6ec3e25e86a7c1564a6f5b32dabbf
Yeison Mosquera <142964641+CtrlJason@users.noreply.github.com>
2024-09-23 21:50:39 -0500

    actualizacion

---
+++ Clase6/prueba/proyecto/tests/test_app.py
@@ -0,0 +1,11 @@
+import unittest
+from mi_proyecto.dominio import Tarea
+
+class TestTarea(unittest.TestCase):
+    def test_crear_tarea(self):
+        tarea = Tarea("Comprar pan", "Ir ala tienda a comprar pan")
+        self.assertEqual(tarea.nombre, "Comprar pan")
+        self.assertEqual(tarea.descripcion, "Ir a la tienda a comprar pan")
+
+if __name__ == "__main__":
+    unittest.main()
